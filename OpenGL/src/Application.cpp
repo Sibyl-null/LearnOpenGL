3dd2b5dc7c501841c33f47163771cf672c264cc0
Sibyl-null <1286438810@qq.com>
2023-06-12 21:08:44 +0800

    [feat]: 每帧清除颜色缓冲

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -29,6 +29,9 @@ int main(void)
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
 
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
