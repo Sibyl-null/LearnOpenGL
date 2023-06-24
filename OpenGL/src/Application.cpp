0de0898d8e99f5ec14b259d68abc08622ba608af
Sibyl-null <1286438810@qq.com>
2023-06-24 15:35:07 +0800

    [feat]: 绘制一堆立方体

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -85,6 +85,19 @@ int main(void)
             -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
         };
 
+        glm::vec3 cubePositions[] = {
+            glm::vec3(0.0f,  0.0f,  0.0f),
+            glm::vec3(2.0f,  5.0f, -15.0f),
+            glm::vec3(-1.5f, -2.2f, -2.5f),
+            glm::vec3(-3.8f, -2.0f, -12.3f),
+            glm::vec3(2.4f, -0.4f, -3.5f),
+            glm::vec3(-1.7f,  3.0f, -7.5f),
+            glm::vec3(1.3f, -2.0f, -2.5f),
+            glm::vec3(1.5f,  2.0f, -2.5f),
+            glm::vec3(1.5f,  0.2f, -1.5f),
+            glm::vec3(-1.3f,  1.0f, -1.5f)
+        };
+
         VertexArray va;
         VertexBuffer vb(vertices, sizeof(vertices));
 
@@ -109,16 +122,21 @@ int main(void)
             texture1.Bind(0);
             texture2.Bind(1);
 
-            glm::mat4 model, view, projection;
-            model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+            glm::mat4 view, projection;
             view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
             projection = glm::perspective(glm::radians(45.0f), (float)Scr_Width / (float)Scr_Height, 0.1f, 100.0f);
 
-            shader.SetUniformMat4f("model", 1, GL_FALSE, glm::value_ptr(model));
             shader.SetUniformMat4f("view", 1, GL_FALSE, glm::value_ptr(view));
             shader.SetUniformMat4f("projection", 1, GL_FALSE, glm::value_ptr(projection));
 
-            renderer.DrawArrays(va, shader, 36);
+            for (int i = 0; i < 10; ++i) {
+                glm::mat4 model;
+                model = glm::translate(model, cubePositions[i]);
+                model = glm::rotate(model, glm::radians(20.0f * i), glm::vec3(1.0f, 0.3f, 0.5f));
+                shader.SetUniformMat4f("model", 1, GL_FALSE, glm::value_ptr(model));
+
+                renderer.DrawArrays(va, shader, 36);
+            }
 
             GLCall(glfwSwapBuffers(window));
             GLCall(glfwPollEvents());
