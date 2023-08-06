e5493f34c2ae8c2cb03344a3a7828066b3b7f8d5
Sibyl-null <1286438810@qq.com>
2023-08-06 16:38:17 +0800

    [feat]: Blinn-Phong 模型模型

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -31,80 +31,38 @@ static float deltaTime = 0.0f;  // 当前帧与上一帧的时间差
 static float lastTime = 0.0f;   // 上一帧的时间
 
 static Camera camera(glm::vec3(0.0f, 5.0f, 25.0f));
+static glm::vec3 lightPos(0.0f, 0.0f, 0.0f);
 
-static glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+static bool blinn = true;
 
 int main(void)
 {
     GLFWwindow* window = OpenGLInit();
 
-    unsigned int amount = 10000;
-    glm::mat4* modelMatrices;
-    modelMatrices = new glm::mat4[amount];
+    float planeVertices[] = {
+        // positions            // normals         // texcoords
+         10.0f, -0.5f,  10.0f,  0.0f, 1.0f, 0.0f,  10.0f,  0.0f,
+        -10.0f, -0.5f,  10.0f,  0.0f, 1.0f, 0.0f,   0.0f,  0.0f,
+        -10.0f, -0.5f, -10.0f,  0.0f, 1.0f, 0.0f,   0.0f, 10.0f,
 
-    srand(glfwGetTime());
-    float radius = 50.0f;
-    float offset = 2.5f;
-    for (unsigned int i = 0; i < amount; ++i) {
-        glm::mat4 model;
-        // 1. 位移：分布在半径为 'radius' 的圆形上，偏移的范围是 [-offset, offset]
-        float angle = (float)i / (float)amount * 360.0f;
-        float displacement = (rand() % (int)(2 * offset * 100)) / 100.0f - offset;
-        float x = sin(angle) * radius + displacement;
-        displacement = (rand() % (int)(2 * offset * 100)) / 100.0f - offset;
-        float y = displacement * 0.4f;      // 让行星带的高度比x和z的宽度要小
-        displacement = (rand() % (int)(2 * offset * 100)) / 100.0f - offset;
-        float z = cos(angle) * radius + displacement;
-        model = glm::translate(model, glm::vec3(x, y, z));
-
-        // 2. 缩放：在 0.05 和 0.25f 之间缩放
-        float scale = (rand() % 20) / 100.0f + 0.05;
-        model = glm::scale(model, glm::vec3(scale));
-
-        // 3. 旋转：绕着一个（半）随机选择的旋转轴向量进行随机的旋转
-        float rotAngle = (rand() % 360);
-        model = glm::rotate(model, rotAngle, glm::vec3(0.4f, 0.6f, 0.8f));
-
-        // 4. 添加到矩阵的数组中
-        modelMatrices[i] = model;
-    }
+         10.0f, -0.5f,  10.0f,  0.0f, 1.0f, 0.0f,  10.0f,  0.0f,
+        -10.0f, -0.5f, -10.0f,  0.0f, 1.0f, 0.0f,   0.0f, 10.0f,
+         10.0f, -0.5f, -10.0f,  0.0f, 1.0f, 0.0f,  10.0f, 10.0f
+    };
 
     // 一个新的作用域，让VertexBuffer/IndexBuffer的析构发生在glfwTerminate之前
     // glfwTerminate调用之后，opengl上下文销毁，glGetError会一直返回一个错误，使GLClearError方法进入死循环
     {
-        Shader modelShader("res/shaders/ModelLoading.shader");
-        Shader instancedShader("res/shaders/Instanced.shader");
+        Shader lightShader("res/shaders/Light.shader");
+        Texture woodTexture("res/textures/wood.png", TextureType::texture_diffuse);
 
-        Model planet("res/models/planet/planet.obj");
-        Model rock("res/models/rock/rock.obj");
-
-        // ---------------------------------------------------
-
-        unsigned int buffer;
-        GLCall(glGenBuffers(1, &buffer));
-        GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
-        GLCall(glBufferData(GL_ARRAY_BUFFER, amount * sizeof(glm::mat4), &modelMatrices[0], GL_STATIC_DRAW));
-
-        for (unsigned int i = 0; i < rock.meshes.size(); ++i) {
-            unsigned int vao = rock.meshes[i].GetVAO();
-            GLCall(glBindVertexArray(vao));
-
-            GLCall(glEnableVertexAttribArray(2));
-            GLCall(glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)0));
-            GLCall(glEnableVertexAttribArray(3));
-            GLCall(glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(sizeof(glm::vec4))));
-            GLCall(glEnableVertexAttribArray(4));
-            GLCall(glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(2 * sizeof(glm::vec4))));
-            GLCall(glEnableVertexAttribArray(5));
-            GLCall(glVertexAttribPointer(5, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(3 * sizeof(glm::vec4))));
-
-            GLCall(glVertexAttribDivisor(2, 1));
-            GLCall(glVertexAttribDivisor(3, 1));
-            GLCall(glVertexAttribDivisor(4, 1));
-            GLCall(glVertexAttribDivisor(5, 1));
-
-            GLCall(glBindVertexArray(0));
-        }
+        VertexArray planeVAO;
+        VertexBuffer planeVBO(planeVertices, sizeof(planeVertices));
+        VertexBufferLayout layout;
+        layout.Push<float>(3);
+        layout.Push<float>(3);
+        layout.Push<float>(2);
+        planeVAO.AddBuffer(planeVBO, layout);
 
         // ---------------------------------------------------
 
@@ -120,40 +78,28 @@ int main(void)
             renderer.Clear();
             // ------------------------------------------------
 
-            glm::mat4 view = camera.GetViewMatrix();
             glm::mat4 projection = glm::perspective(glm::radians(camera.GetFoV()), 
-                (float)Scr_Width / (float)Scr_Height, 0.1f, 1000.0f);
-            glm::mat4 model;
-            model = glm::translate(model, glm::vec3(0.0f, -3.0f, 0.0f));
-            model = glm::scale(model, glm::vec3(4.0f, 4.0f, 4.0f));
+                (float)Scr_Width / (float)Scr_Height, 0.1f, 100.0f);
+            glm::mat4 view = camera.GetViewMatrix();
 
-            modelShader.Bind();
-            modelShader.SetUniformMat4f("model", model);
-            modelShader.SetUniformMat4f("view", view);
-            modelShader.SetUniformMat4f("projection", projection);
-            planet.Draw(modelShader);
+            lightShader.Bind();
+            lightShader.SetUniformMat4f("view", view);
+            lightShader.SetUniformMat4f("projection", projection);
+            lightShader.SetUniform1i("floorTexture", 0);
+            lightShader.SetUniform3f("lightPos", lightPos);
+            lightShader.SetUniform3f("viewPos", camera.GetPosition());
+            lightShader.SetUniform1i("blinn", blinn);
 
+            woodTexture.Bind(0);
+            renderer.DrawArrays(planeVAO, lightShader, sizeof(planeVertices) / sizeof(float));
 
-            instancedShader.Bind();
-            instancedShader.SetUniformMat4f("view", view);
-            instancedShader.SetUniformMat4f("projection", projection);
-            instancedShader.SetUniform1i("texture_diffuse1", 0);
-            GLCall(glActiveTexture(GL_TEXTURE0));
-            rock.textures_loaded[0].Bind(0);
-
-            for (unsigned int i = 0; i < rock.meshes.size(); ++i) {
-                GLCall(glBindVertexArray(rock.meshes[i].GetVAO()));
-                GLCall(glDrawElementsInstanced(GL_TRIANGLES, rock.meshes[i].indices.size(),
-                    GL_UNSIGNED_INT, 0, amount));
-            }
+            std::cout << (blinn ? "Blinn-Phong" : "Phong") << std::endl;
 
             // ------------------------------------------------
             GLCall(glfwSwapBuffers(window));
             GLCall(glfwPollEvents());
         }
     }
-
-    delete[] modelMatrices;
     
     glfwTerminate();
     return 0;
@@ -191,6 +137,9 @@ void ProcessInput(GLFWwindow* window) {
     // 按下 ECS 键，就关闭窗口
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS)
+        blinn = !blinn;
 
     camera.ProcessKeyboard(window, deltaTime);
 }
