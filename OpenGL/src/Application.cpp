122968d813f9a6a781e846f9d746b434a0da555f
Sibyl-null <1286438810@qq.com>
2023-08-07 23:31:38 +0800

    [feat]: ShaderMapping - 生成深度贴图

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -23,6 +23,7 @@ void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
 void ProcessInput(GLFWwindow* window);
 void MouseCallback(GLFWwindow* window, double xpos, double ypos);
 void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+void RenderScene(Shader& shader, Renderer& renderer, VertexArray& planeVAO, VertexArray& cubeVAO);
 
 const unsigned int Scr_Width = 800;
 const unsigned int Scr_Height = 600;
@@ -30,10 +31,8 @@ const unsigned int Scr_Height = 600;
 static float deltaTime = 0.0f;  // 当前帧与上一帧的时间差
 static float lastTime = 0.0f;   // 上一帧的时间
 
-static Camera camera(glm::vec3(0.0f, 5.0f, 25.0f));
-static glm::vec3 lightPos(0.0f, 0.0f, 0.0f);
-
-static bool blinn = true;
+static Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+static glm::vec3 lightPos(-2.0f, 4.0f, -1.0f);
 
 int main(void)
 {
@@ -41,28 +40,124 @@ int main(void)
 
     float planeVertices[] = {
         // positions            // normals         // texcoords
-         10.0f, -0.5f,  10.0f,  0.0f, 1.0f, 0.0f,  10.0f,  0.0f,
-        -10.0f, -0.5f,  10.0f,  0.0f, 1.0f, 0.0f,   0.0f,  0.0f,
-        -10.0f, -0.5f, -10.0f,  0.0f, 1.0f, 0.0f,   0.0f, 10.0f,
+         25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
+        -25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,   0.0f,  0.0f,
+        -25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
 
-         10.0f, -0.5f,  10.0f,  0.0f, 1.0f, 0.0f,  10.0f,  0.0f,
-        -10.0f, -0.5f, -10.0f,  0.0f, 1.0f, 0.0f,   0.0f, 10.0f,
-         10.0f, -0.5f, -10.0f,  0.0f, 1.0f, 0.0f,  10.0f, 10.0f
+         25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
+        -25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
+         25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,  25.0f, 25.0f
+    };
+
+    float cubeVertices[] = {
+        // back face
+        -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+         1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
+         1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 0.0f, // bottom-right         
+         1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
+        -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+        -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 1.0f, // top-left
+        // front face
+        -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
+         1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 0.0f, // bottom-right
+         1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
+         1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
+        -1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 1.0f, // top-left
+        -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
+        // left face
+        -1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
+        -1.0f,  1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-left
+        -1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
+        -1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
+        -1.0f, -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-right
+        -1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
+        // right face
+         1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
+         1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
+         1.0f,  1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-right         
+         1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
+         1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
+         1.0f, -1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-left     
+        // bottom face
+        -1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
+         1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 1.0f, // top-left
+         1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
+         1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
+        -1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 0.0f, // bottom-right
+        -1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
+        // top face
+        -1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
+         1.0f,  1.0f , 1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
+         1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 1.0f, // top-right     
+         1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
+        -1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
+        -1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 0.0f  // bottom-left        
+    };
+
+    float quadVertices[] = {
+        // positions        // texture Coords
+        -1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
+        -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+         1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
+
+         1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
+         -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+         1.0f, -1.0f, 0.0f, 1.0f, 0.0f
     };
 
     // 一个新的作用域，让VertexBuffer/IndexBuffer的析构发生在glfwTerminate之前
     // glfwTerminate调用之后，opengl上下文销毁，glGetError会一直返回一个错误，使GLClearError方法进入死循环
     {
-        Shader lightShader("res/shaders/Light.shader");
-        Texture woodTexture("res/textures/wood.png", TextureType::texture_diffuse);
-
         VertexArray planeVAO;
         VertexBuffer planeVBO(planeVertices, sizeof(planeVertices));
-        VertexBufferLayout layout;
-        layout.Push<float>(3);
-        layout.Push<float>(3);
-        layout.Push<float>(2);
-        planeVAO.AddBuffer(planeVBO, layout);
+        VertexBufferLayout planeLayout;
+        planeLayout.Push<float>(3);
+        planeLayout.Push<float>(3);
+        planeLayout.Push<float>(2);
+        planeVAO.AddBuffer(planeVBO, planeLayout);
+
+        VertexArray cubeVAO;
+        VertexBuffer cubeVBO(cubeVertices, sizeof(cubeVertices));
+        VertexBufferLayout cubeLayout;
+        cubeLayout.Push<float>(3);
+        cubeLayout.Push<float>(3);
+        cubeLayout.Push<float>(2);
+        cubeVAO.AddBuffer(cubeVBO, cubeLayout);
+
+        VertexArray quadVAO;
+        VertexBuffer quadVBO(quadVertices, sizeof(quadVertices));
+        VertexBufferLayout quadLayout;
+        quadLayout.Push<float>(3);
+        quadLayout.Push<float>(2);
+        quadVAO.AddBuffer(quadVBO, quadLayout);
+
+        // ---------------------------------------------------
+
+        Shader simpleDepthShader("res/shaders/SimpleDepth.shader");
+        Shader debugDepthQuadShader("res/shaders/DebugDepthQuad.shader");
+
+        // Texture woodTexture("res/textures/wood.png", TextureType::texture_diffuse);
+
+        // ---------------------------------------------------
+
+        const unsigned int Shadow_Width = 1024, Shadow_Height = 1024;
+        unsigned int depthMapFBO;
+        GLCall(glGenFramebuffers(1, &depthMapFBO));
+
+        unsigned int depthMap;
+        GLCall(glGenTextures(1, &depthMap));
+        GLCall(glBindTexture(GL_TEXTURE_2D, depthMap));
+        GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, Shadow_Width, Shadow_Height, 
+            0, GL_DEPTH_COMPONENT, GL_FLOAT, nullptr));
+        GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST));
+        GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
+        GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT));
+        GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT));
+
+        GLCall(glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO));
+        GLCall(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0));
+        GLCall(glDrawBuffer(GL_NONE));
+        GLCall(glReadBuffer(GL_NONE));
 
         // ---------------------------------------------------
 
@@ -78,22 +173,31 @@ int main(void)
             renderer.Clear();
             // ------------------------------------------------
 
-            glm::mat4 projection = glm::perspective(glm::radians(camera.GetFoV()), 
-                (float)Scr_Width / (float)Scr_Height, 0.1f, 100.0f);
-            glm::mat4 view = camera.GetViewMatrix();
+            // 1. 生成深度贴图
+            float near_plane = 1.0f, far_plane = 7.5f;
+            glm::mat4 lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane);
+            glm::mat4 lightView = glm::lookAt(lightPos, glm::vec3(0.0), glm::vec3(0.0, 1.0, 0.0));
+            glm::mat4 lightSpaceMatrix = lightProjection * lightView;
 
-            lightShader.Bind();
-            lightShader.SetUniformMat4f("view", view);
-            lightShader.SetUniformMat4f("projection", projection);
-            lightShader.SetUniform1i("floorTexture", 0);
-            lightShader.SetUniform3f("lightPos", lightPos);
-            lightShader.SetUniform3f("viewPos", camera.GetPosition());
-            lightShader.SetUniform1i("blinn", blinn);
+            simpleDepthShader.Bind();
+            simpleDepthShader.SetUniformMat4f("lightSpaceMatrix", lightSpaceMatrix);
 
-            woodTexture.Bind(0);
-            renderer.DrawArrays(planeVAO, lightShader, sizeof(planeVertices) / sizeof(float));
+            GLCall(glViewport(0, 0, Shadow_Width, Shadow_Height));
+            GLCall(glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO));
+            GLCall(glClear(GL_DEPTH_BUFFER_BIT));
 
-            std::cout << (blinn ? "Blinn-Phong" : "Phong") << std::endl;
+            RenderScene(simpleDepthShader, renderer, planeVAO, cubeVAO);
+            GLCall(glBindFramebuffer(GL_FRAMEBUFFER, 0));
+
+            // 2. 将深度贴图渲染出来
+            GLCall(glViewport(0, 0, Scr_Width, Scr_Height));
+            GLCall(glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT));
+
+            debugDepthQuadShader.Bind();
+            debugDepthQuadShader.SetUniform1i("depthMap", 0);
+            GLCall(glActiveTexture(GL_TEXTURE0));
+            GLCall(glBindTexture(GL_TEXTURE_2D, depthMap));
+            renderer.DrawArrays(quadVAO, debugDepthQuadShader, 6);
 
             // ------------------------------------------------
             GLCall(glfwSwapBuffers(window));
@@ -138,9 +242,6 @@ void ProcessInput(GLFWwindow* window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 
-    if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS)
-        blinn = !blinn;
-
     camera.ProcessKeyboard(window, deltaTime);
 }
 
@@ -150,4 +251,32 @@ void MouseCallback(GLFWwindow* window, double xpos, double ypos) {
 
 void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset) {
     camera.ProcessMouseScroll(yoffset);
+}
+
+void RenderScene(Shader& shader, Renderer& renderer, VertexArray& planeVAO, VertexArray& cubeVAO) {
+    // floor
+    glm::mat4 model = glm::mat4(1.0f);
+    shader.SetUniformMat4f("model", model);
+    planeVAO.Bind();
+    renderer.DrawArrays(planeVAO, shader, 6);
+    // cubes
+    model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(0.0f, 1.5f, 0.0));
+    model = glm::scale(model, glm::vec3(0.5f));
+    shader.SetUniformMat4f("model", model);
+    cubeVAO.Bind();
+    renderer.DrawArrays(cubeVAO, shader, 36);
+
+    model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(2.0f, 0.0f, 1.0));
+    model = glm::scale(model, glm::vec3(0.5f));
+    shader.SetUniformMat4f("model", model);
+    renderer.DrawArrays(cubeVAO, shader, 36);
+
+    model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(-1.0f, 0.0f, 2.0));
+    model = glm::rotate(model, glm::radians(60.0f), glm::normalize(glm::vec3(1.0, 0.0, 1.0)));
+    model = glm::scale(model, glm::vec3(0.25));
+    shader.SetUniformMat4f("model", model);
+    renderer.DrawArrays(cubeVAO, shader, 36);
 }
