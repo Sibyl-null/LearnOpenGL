6b0b486712d186022179c3158b2989b7c733f9fc
Sibyl-null <1286438810@qq.com>
2023-08-08 22:57:55 +0800

    [feat]: ShadowMapping - 采样过多

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -152,8 +152,10 @@ int main(void)
             0, GL_DEPTH_COMPONENT, GL_FLOAT, nullptr));
         GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST));
         GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
-        GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT));
-        GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT));
+        GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER));
+        GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER));
+        float borderColor[] = { 1.0, 1.0, 1.0, 1.0 };
+        GLCall(glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor));
 
         GLCall(glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO));
         GLCall(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0));
